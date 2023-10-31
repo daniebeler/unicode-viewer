87c111ab910dc2d2d693870ff4c836388a04c3aa
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2023-10-31 08:50:33 +0100

    showing data in table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,25 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {[...Array(3000)].map((x, i) => {
+      <table>
+        <tr className="sticky top-0">
+          <th>Symbol</th>
+          <th>Unicode-Nummer</th>
+          <th>HTML-Code</th>
+        </tr>
+        {[...Array(3000)].map((x, i) => {
+          const fief = String.fromCodePoint(i);
+          console.log(fief);
 
-        const fief = String.fromCodePoint(i)
-        console.log(fief)
-
-        return (
-          <h1 key={i}>{fief + " " + i}</h1>
-        )
-  })}
+          return (
+          <tr key={i}>
+            <td>{fief}</td>
+            <td>{'U+' + i.toString(16).padStart(4, '0').toUpperCase()}</td>
+            <td>{'&#' + i + ';'}</td>
+          </tr>
+          )
+        })}
+      </table>
     </main>
   );
 }
